fc8b6b81fa21a8af2f110b592e800c0c044d260f
Reflection0813 <imitation0813@gmail.com>
2016-12-09 17:29:06 +0900

    complete stage 1,2

--- Assets/Scripts/Main/JumpScript.cs
+++ Assets/Scripts/Main/JumpScript.cs
@@ -8,7 +8,7 @@ public class JumpScript : MonoBehaviour {
 	void OnCollisionEnter2D(Collision2D col){
 		if (col.gameObject.tag == "Player") {
 			Sound.PlaySe ("jump");
-			col.gameObject.GetComponent<Rigidbody2D> ().AddForce (new Vector2 (0, 800));
+			col.gameObject.GetComponent<Rigidbody2D> ().AddForce (new Vector2 (0, 700));
 		}
 	}
 }
