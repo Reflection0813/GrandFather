9642ee8eb3dd26073d67ccfc236963ced5c5732b
Reflection0813 <imitation0813@gmail.com>
2016-12-24 06:01:41 +0900

    4th stage added

---
+++ Assets/Scripts/Main/LiftScript.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LiftScript : MonoBehaviour {
+	//上下のときはtrue
+	private bool vhFlag;
+	private int direction;
+
+	private Vector3 defaultPosition;
+	private Vector3[] targetPosition = new Vector3[2];
+	private Vector3 x_speed;
+	private Vector3 y_speed;
+	public float movingSize;
+
+
+	// Use this for initialization
+	void Start () {
+		x_speed = new Vector3(0.05f,0,0);
+		y_speed = new Vector3 (0, 0.05f, 0);
+
+		targetPosition[0] = targetPosition[1] = defaultPosition = gameObject.transform.position;
+
+
+		if (gameObject.tag == "udLift") {
+			vhFlag = true;
+			targetPosition [0].x = defaultPosition.x - movingSize;
+			targetPosition [1].x = defaultPosition.x + movingSize;
+		} else {
+			vhFlag = false;
+			targetPosition [0].y = defaultPosition.y - movingSize;
+			targetPosition [1].y = defaultPosition.y + movingSize;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (vhFlag) {
+			transform.position = Vector3.Lerp (transform.position,targetPosition[direction],Time.deltaTime * 0.2f);
+
+			if (gameObject.transform.position.y >= defaultPosition.y + movingSize)
+				direction = 0;
+			if (gameObject.transform.position.y <= defaultPosition.y - movingSize + 10)
+				direction = 1;
+				
+			
+		} else {
+			transform.position = Vector3.Lerp (transform.position,targetPosition[direction],Time.deltaTime * 0.2f);
+
+			if (gameObject.transform.position.x >= defaultPosition.x + movingSize - 1)
+				direction = 0;
+			if (gameObject.transform.position.x <= defaultPosition.x - movingSize + 1)
+				direction = 1;
+		}
+
+
+	}
+}
---
+++ Assets/Scripts/Main/SimpleLiftScript.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleLiftScript : MonoBehaviour {
+	//上下のときはtrue
+	private bool vhFlag;
+	private bool direction;
+
+	private Vector3 defaultPosition;
+	private Vector3 x_speed;
+	private Vector3 y_speed;
+
+	public float movingSize;
+	public float speed;
+
+
+	// Use this for initialization
+	void Start () {
+
+
+
+		if (gameObject.tag == "udLift") {
+			vhFlag = true;
+			y_speed = new Vector3 (0, speed, 0);
+		} else {
+			vhFlag = false;
+			x_speed = new Vector3 (speed, 0, 0);
+		}
+
+		defaultPosition = gameObject.transform.position;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (vhFlag) {
+			if (direction)
+				transform.position += y_speed;
+			else
+				transform.position -= y_speed;
+
+			if (transform.position.y >= defaultPosition.y + movingSize || transform.position.y <= defaultPosition.y - movingSize)
+				direction = !direction;
+
+
+		} else {
+			if (direction)
+				transform.position += x_speed;
+			else
+				transform.position -= x_speed;
+
+			if (transform.position.x >= defaultPosition.x + movingSize || transform.position.x <= defaultPosition.x - movingSize)
+				direction = !direction;
+		}
+
+
+	}
+}
